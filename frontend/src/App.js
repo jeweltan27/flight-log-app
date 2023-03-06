1a92aab83ad3de40f3ce50230eb78a0ea0cf1ce6
jeweltan27 <jewelxyt@gmail.com>
2023-03-06 23:55:14 +0800

    Feature: Add routes for login and registration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./components/Header";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import Registration from './pages/Registration';
 
 function App() {
   return (
     <div className="App">
         <Header />
         <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/register" element={<Registration />} />
+
         </Routes>
     </div>
   );
