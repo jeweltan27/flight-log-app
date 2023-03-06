da5b5c0bd4e690a18811e9a3418aef4a62246b9d
jeweltan27 <jewelxyt@gmail.com>
2023-03-06 22:23:39 +0800

    Create React app with home page

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Header from "./components/Header";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div className="App">
+
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import FlightLogs from '../components/FlightLogs'
+import "../assets/Home.css";
+
+const Home = () => {
+    const flightlogs = [
+        {
+            tailNumber: "001",
+            flightID: "SQ351",
+            takeoff: "2023-02-15T00:00:00+08:00",
+            landing: "2023-02-15T00:12:30+08:00",
+            duration: "12h 30 min",
+        },
+        {
+            tailNumber: "002",
+            flightID: "SQ352",
+            takeoff: "2023-02-17T23:00:00+08:00",
+            landing: "2023-02-18T00:12:15+08:00",
+            duration: "13h 15 min",
+        }
+    ]
+    return (
+        <div className="home">
+            <h2>
+                Flight Logs
+            </h2>
+        </div>
+    )
+}
+
+export default Home
