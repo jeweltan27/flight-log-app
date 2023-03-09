0b2724c5632524241de044efd9aeff7ee98c617c
jeweltan27 <jewelxyt@gmail.com>
2023-03-10 01:40:56 +0800

    Update: CSS styles

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from 'react';
 import Logo from "../assets/logo.png"; 
 
 const Header = () => {
   return (
-    <nav className="navbar navbar-expand navbar-light">
-        <img src={Logo} width="110px" alt="Company Logo" />
+    <nav className="navbar navbar-expand navbar-light mb-3">
+        <img className="logo" src={Logo} width="130px" alt="Company Logo" />
     </nav>
   )
 }
